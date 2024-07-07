2f0bc1d805db8a41f4a5ef425510fbb91be7ad0c
AlirezaNoor <alirezang7575@gmail.com>
2024-07-07 01:30:00 -0400

    add GRPC to project

---
+++ IdentityService/IdentityService/GrpcServices/PermissionService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using IdentiyService.Protos;
+
+namespace Identity.API.GrpcServices
+{
+    public class PermissionService : Permission.PermissionBase
+    {
+        public override async Task<CheckPermissionResponse> CheckPermission(CheckPermissionRequest request, ServerCallContext context)
+        {
+            var temp = context.AuthContext.ToString();
+            var response = new CheckPermissionResponse();
+            //call business service
+            if (request.Permission == "product_get_all" && request.Applicationid == "catalog")
+            {
+                response.Allowed = true;
+            }
+            else
+            {
+                response.Allowed = false;
+            }
+            return await Task.FromResult(response);
+        }
+    }
+}
